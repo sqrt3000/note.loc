7df7a556edc5b7ce22447a96fcfe0305ee8ab0bb
Alex <etercity@gmail.com>
2019-10-09 14:35:39 +0300

    Alex_Eter

--- notebook.sql
+++ notebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 09 2019 г., 13:05
+-- Время создания: Окт 09 2019 г., 14:33
 -- Версия сервера: 5.6.38
 -- Версия PHP: 7.1.12
 
@@ -41,14 +41,7 @@ CREATE TABLE `abonent` (
 --
 
 INSERT INTO `abonent` (`id`, `name`, `phone`, `email`, `user_id`) VALUES
-(7, 'qwer555', '0674138256', 'etercity@gmail.com', 2),
-(17, 'Манукян Александр Семенович', '0674138256', 'krrt.ua@gmail.com', 3),
-(18, 'Alex', '5555', 'etercity@gmail.com', 2),
-(19, 'Aleha23', '0674138256', 'olgakorobenko@meta.ua', 2),
-(22, '', '', '', 0),
-(24, '', '', '', 0),
-(25, 'Alexandr', '222122', 'etervideo@gmail.com', 2),
-(26, 'Alexandr', '0961191727', 'info@cheernika.com.ua', 2);
+(17, 'Манукян Александр Семенович', '0674138256', 'etercity@gmail.com', 3);
 
 -- --------------------------------------------------------
 
@@ -67,8 +60,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `login`, `password`) VALUES
-(2, 'test', '$2y$10$R2knchwYCdidET0cdNR3U.U9YoN3J2m5E05GwMD7tBge7wILW19ny'),
-(3, 'admin', '$2y$10$PcY0frOGV60a1ilzwneNa.kh.eE0I/U1orkJcL49kSz3vngYeBna2');
+(2, 'test', '$2y$10$R2knchwYCdidET0cdNR3U.U9YoN3J2m5E05GwMD7tBge7wILW19ny');
 
 --
 -- Индексы сохранённых таблиц
@@ -95,13 +87,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `abonent`
 --
 ALTER TABLE `abonent`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
